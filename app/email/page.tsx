b58eb3dd3158a0ec2d7059077ae01df6fdf68b76
Hemit Patel <hemitvpatel@gmail.com>
2026-02-02 20:31:29 -0500

    remove react

--- app/email/page.tsx
+++ app/email/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { handleSubscribeEmail } from '@/server-actions/sub-to-email';
 import topics from '@/data/topic-options';
 
